ac4c5cff6d3322ba727bb2bae84cc57c5a3e566a
awadbilal <awadbilal99@gmail.com>
2024-02-10 20:10:01 +0300

    Initialization and upload

---
+++ config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  app: {
+    token: "YOUR_BOT_TOKEN_GOES_HERE",
+    playing: "by awadbilal ❤️",
+    global: true,
+    guild: "",
+    ExtraMessages: false,
+    loopMessage: false,
+  },
+
+  opt: {
+    DJ: {
+      enabled: false,
+      roleName: "",
+      commands: [],
+    },
+    maxVol: 100,
+    spotifyBridge: true,
+    volume: 75,
+    leaveOnEmpty: true,
+    leaveOnEmptyCooldown: 30000,
+    leaveOnEnd: true,
+    leaveOnEndCooldown: 30000,
+    discordPlayer: {
+      ytdlOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25,
+      },
+    },
+  },
+};
